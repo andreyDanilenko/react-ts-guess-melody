bde5b3d2776d5dfc44439b1cc4579f850ffe95ec
andreyDanilenko <danilenko.a.g@mail.ru>
2022-07-13 20:39:59 +0300

    add psge

---
+++ project/src/pages/index.ts
@@ -0,0 +1,7 @@
+import GameOverPage from './gameOverPage/GameOverPage';
+import WinPage from './winPage/WinPage';
+import GamePage from './gamePage/GamePage';
+import WelcomePage from './welcomePage/WelcomePage';
+import LoginPage from './loginPage/LoginPage';
+
+export {GameOverPage, WinPage, GamePage, WelcomePage, LoginPage};
